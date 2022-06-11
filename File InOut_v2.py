0e77a185bac7a9033a061b19ba4ca96a7149139e
Egor Vasinev <106512748+TheAlh@users.noreply.github.com>
2022-06-11 19:56:17 +0700

    Update File InOut_v2.py
    Comments translated

--- File InOut_v2.py
+++ File InOut_v2.py
@@ -1,18 +1,18 @@
 max_str = {}
-with open('C:/Test Space of Python/test.txt', 'r') as inf: #Считываем документ
+with open('C:/Test Space of Python/test.txt', 'r') as inf: #Reading the file
     for line in inf:
-        text = list(line.strip().split(' ')) #Вводим строку в переменную text
+        text = list(line.strip().split(' ')) #Enter a string into the text variable
         for i in range(len(text)):
             if str.lower(text[i]) in max_str:
-                max_str[str.lower(text[i])] += 1 #Если значение строки уже есть в ключе словаря, то увеличиваем значение на один
+                max_str[str.lower(text[i])] += 1 #If the given string value is already in the dictionary key, then increase the value in the dictionary by one
             else:
-                max_str[str.lower(text[i])] = 1 #Если значения строки нет в словаре, то создаем ключ со значением равным единице
+                max_str[str.lower(text[i])] = 1 #Else create a key with value equal to one
 
 with open('C:/Test Space of Python/out.txt', 'w') as ouf:
     values, key = 0, ' '
-    for k, v in max_str.items(): #Передаем значение и ключ из словаря
-        if values < v:  #В данном блоке определяем самое больше значение и его ключ
+    for k, v in max_str.items(): #Passing the value and key from the dictionary
+        if values < v: #In this block, we define the largest value and its key
             values = v
             key = k
-    ouf.write(f'{key} {values}\n') #Выводим строку в файл
-    values, key = 0, ' ' #Обнуляем переменные для сравнения
+    ouf.write(f'{key} {values}\n') #Output string to file
+    values, key = 0, ' ' #Reset variables for comparison
