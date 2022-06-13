3208af9e1b43b6fb234009822cb8ffabdf58f54d
TheAlh <thealh@yandex.com>
2022-06-13 22:45:41 +0700

    Update Task with Requests.py

--- Task with Requests.py
+++ Task with Requests.py
@@ -1,14 +1,4 @@
 import requests
 
-num = 0
-with open('C:/Test Space of Python/test.txt', 'r') as inf:  #Read file
-    for line in inf:
-        link = requests.get(line.strip())   #Output a link in variable
-
-with open('C:/Test Space of Python/download.txt', 'wb') as download:    #Download file
-    download.write(link.content)
-
-with open('C:/Test Space of Python/download.txt', 'r') as inf:  #Reading a file and counting the number of lines in it
-    for _ in inf:
-        num += 1
-print(num)
+with open('C:/Test Space of Python/test.txt', 'r') as inf:
+    print(len(requests.get(inf.readline().strip()).text.splitlines()))
